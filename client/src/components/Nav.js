e7e129f0102f0e4416b2ad95bd995fb6c2ceba53
manojnaraharisetti <2100032451@kluniversity.in>
2023-07-24 16:56:52 +0530

    sidebar redefined

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,30 +1,87 @@
-import React from "react";
+import React,{useState} from "react";
 import './Styles/Navstyle.css';
 import Navimg from './Navimg/logo.png';
 import userico from './Navimg/face-1.png';
 
 export default function Nav() {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const shrinkBtnHandler = () => {
+    document.body.classList.toggle("shrink");
+    setTimeout(moveActiveTab, 400);
+
+    const shrinkBtn = document.querySelector(".shrink-btn");
+    shrinkBtn.classList.add("hovered");
+
+    setTimeout(() => {
+      shrinkBtn.classList.remove("hovered");
+    }, 500);
+  };
+
+  const searchHandler = () => {
+    document.body.classList.remove("shrink");
+    const search = document.querySelector(".search");
+    search.lastElementChild.focus();
+  };
+
+  const moveActiveTab = () => {
+    let topPosition = activeIndex * 58 + 2.5;
+    const shortcuts = document.querySelector(".sidebar-links h4");
+
+    if (activeIndex > 3) {
+      topPosition += shortcuts.clientHeight;
+    }
+
+    const activeTab = document.querySelector(".active-tab");
+    activeTab.style.top = `${topPosition}px`;
+  };
+
+  const changeLinkHandler = (e) => {
+    const sidebarLinks = document.querySelectorAll(".sidebar-links a");
+    sidebarLinks.forEach((sideLink) =>
+      sideLink.classList.remove("active")
+    );
+    e.target.classList.add("active");
+
+    setActiveIndex(e.target.dataset.active);
+    moveActiveTab();
+  };
+
+  const showTooltipHandler = (e) => {
+    let tooltip = e.target.parentNode.lastElementChild;
+    let spans = tooltip.children;
+    let tooltipIndex = e.target.dataset.tooltip;
+
+    Array.from(spans).forEach((sp) => sp.classList.remove("show"));
+    spans[tooltipIndex].classList.add("show");
+
+    tooltip.style.top = `${
+      (100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)
+    }%`;
+  };
+
   return (
     <div>
       <nav>
         <div className="sidebar-top">
-          <span className="shrink-btn">
+          <span className="shrink-btn" onClick={shrinkBtnHandler}>
             <i className="bx bx-chevron-left"></i>
           </span>
           <img src={Navimg} className="logo" alt="" />
           <h3 className="hide">SONIC</h3>
         </div>
 
-        <div className="search">
+        <div className="search" onClick={searchHandler}>
           <i className="bx bx-search"></i>
           <input type="text" className="hide" placeholder="Quick Search ..." />
         </div>
 
-        <div className="sidebar-links">
+        <div className="sidebar-links" onClick={changeLinkHandler}>
           <ul>
             <div className="active-tab"></div>
-            <li className="tooltip-element" data-tooltip="0">
-              <a href="/" className="active" data-active="0">
+            <li className="tooltip-element" data-tooltip="0" onMouseOver={showTooltipHandler}>
+              <a href="/" className="active" data-active="0" >
                 <div className="icon">
                   <i className="bx bx-tachometer"></i>
                   <i className="bx bxs-tachometer"></i>
@@ -32,8 +89,8 @@ export default function Nav() {
                 <span className="link hide">Home</span>
               </a>
             </li>
-            <li className="tooltip-element" data-tooltip="1">
-              <a href="/" data-active="1">
+            <li className="tooltip-element" data-tooltip="1" onMouseOver={showTooltipHandler}>
+              <a href="/" data-active="1" >
                 <div className="icon">
                   <i className="bx bx-folder"></i>
                   <i className="bx bxs-folder"></i>
@@ -41,8 +98,8 @@ export default function Nav() {
                 <span className="link hide">Messages</span>
               </a>
             </li>
-            <li className="tooltip-element" data-tooltip="2">
-              <a href="/" data-active="2">
+            <li className="tooltip-element" data-tooltip="2" onMouseOver={showTooltipHandler}>
+              <a href="/" data-active="2" >
                 <div className="icon">
                   <i className="bx bx-message-square-detail"></i>
                   <i className="bx bxs-message-square-detail"></i>
@@ -50,8 +107,8 @@ export default function Nav() {
                 <span className="link hide">Requests</span>
               </a>
             </li>
-            <li className="tooltip-element" data-tooltip="3">
-              <a href="/" data-active="3">
+            <li className="tooltip-element" data-tooltip="3" onMouseOver={showTooltipHandler}>
+              <a href="/" data-active="3" >
                 <div className="icon">
                   <i className="bx bx-bar-chart-square"></i>
                   <i className="bx bxs-bar-chart-square"></i>
@@ -68,9 +125,8 @@ export default function Nav() {
           </ul>
 
           <h4 className="hide">Shortcuts</h4>
-
           <ul>
-            <li className="tooltip-element" data-tooltip="0">
+            <li className="tooltip-element" data-tooltip="0" onMouseOver={showTooltipHandler}>
               <a href="/" data-active="4">
                 <div className="icon">
                   <i className="bx bx-notepad"></i>
@@ -79,7 +135,7 @@ export default function Nav() {
                 <span className="link hide">Notifications</span>
               </a>
             </li>
-            <li className="tooltip-element" data-tooltip="1">
+            <li className="tooltip-element" data-tooltip="1" onMouseOver={showTooltipHandler}>
               <a href="/" data-active="5">
                 <div className="icon">
                   <i className="bx bx-help-circle"></i>
@@ -88,7 +144,7 @@ export default function Nav() {
                 <span className="link hide">Help</span>
               </a>
             </li>
-            <li className="tooltip-element" data-tooltip="2">
+            <li className="tooltip-element" data-tooltip="2" onMouseOver={showTooltipHandler}>
               <a href="/" data-active="6">
                 <div className="icon">
                   <i className="bx bx-cog"></i>
@@ -106,10 +162,10 @@ export default function Nav() {
         </div>
 
         <div className="sidebar-footer">
-          <a href="/" className="account tooltip-element" data-tooltip="0">
+          <a href="/" className="account tooltip-element" data-tooltip="0" onMouseOver={showTooltipHandler}>
             <i className="bx bx-user"></i>
           </a>
-          <div className="admin-user tooltip-element" data-tooltip="1">
+          <div className="admin-user tooltip-element" data-tooltip="1" onMouseOver={showTooltipHandler}>
             <div className="admin-profile hide">
               <img src={userico} alt="" />
               <div className="admin-info">
